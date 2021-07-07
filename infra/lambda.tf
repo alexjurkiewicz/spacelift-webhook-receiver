27327ba38f8d9d8f3da742c93afc113fb0b165f9
Alex Jurkiewicz <alex@jurkiewi.cz>
2021-07-07 12:41:22 +1000

    Fully disable DynamoDB

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -30,10 +30,11 @@ resource "aws_iam_role" "lambda" {
   name                = var.name
   assume_role_policy  = data.aws_iam_policy_document.lambda_trust.json
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
-  inline_policy {
-    name   = "DynamoDB"
-    policy = data.aws_iam_policy_document.lambda_dynamodb_access.json
-  }
+  # Disabled for now
+  # inline_policy {
+  #   name   = "DynamoDB"
+  #   policy = data.aws_iam_policy_document.lambda_dynamodb_access.json
+  # }
 }
 
 data "aws_iam_policy_document" "lambda_trust" {
@@ -46,16 +47,16 @@ data "aws_iam_policy_document" "lambda_trust" {
   }
 }
 
-data "aws_iam_policy_document" "lambda_dynamodb_access" {
-  statement {
-    actions = [
-      "dynamodb:GetItem",
-      "dynamodb:BatchGetItem",
-      "dynamodb:DeleteItem",
-      "dynamodb:PutItem",
-      "dynamodb:UpdateItem",
-      "dynamodb:BatchWriteItem",
-    ]
-    resources = [module.dynamodb.table_arn]
-  }
-}
+# data "aws_iam_policy_document" "lambda_dynamodb_access" {
+#   statement {
+#     actions = [
+#       "dynamodb:GetItem",
+#       "dynamodb:BatchGetItem",
+#       "dynamodb:DeleteItem",
+#       "dynamodb:PutItem",
+#       "dynamodb:UpdateItem",
+#       "dynamodb:BatchWriteItem",
+#     ]
+#     resources = [module.dynamodb.table_arn]
+#   }
+# }
