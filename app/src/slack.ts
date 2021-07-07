e31814a5abd26ca316db86f5db8b2f38e8f3c4bd
Alex Jurkiewicz <alex@jurkiewi.cz>
2021-07-07 12:36:41 +1000

    Correct emoji name

--- app/src/slack.ts
+++ app/src/slack.ts
@@ -53,7 +53,7 @@ export function generateSlackMessage(event: SpaceliftWebhookPayload): SlackMessa
     const entries = []
     if (event.run.delta.added > 0) entries.push(`${event.run.delta.added} :new:`)
     if (event.run.delta.changed > 0) entries.push(`${event.run.delta.changed} :arrows_counterclockwise:`)
-    if (event.run.delta.deleted > 0) entries.push(`${event.run.delta.deleted} :bin_your_rubbish:`,)
+    if (event.run.delta.deleted > 0) entries.push(`${event.run.delta.deleted} :put_litter_in_its_place:`,)
     if (entries.length === 0) entries.push("No changes")
     const changes: MrkdwnElement = {
       type: "mrkdwn",
